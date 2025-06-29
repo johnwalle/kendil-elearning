917c6258fcbd98d0c06f3d1c2b5492f064ecab69
mark133227 <etawe1627@gmail.com>
2025-06-28 14:36:15 -1200

    add signup and login page

---
+++ src/app/auth/signup/page.tsx
@@ -0,0 +1,9 @@
+import AuthForm from '@/components/auth/AuthForm';
+
+export default function SignUpPage() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-blue-200 to-blue-500">
+      <AuthForm />
+    </div>
+  );
+}
